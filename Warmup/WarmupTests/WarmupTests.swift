6f92c876febee0b2d348ac35af7053b7a55cb8ca
Russ Christensen <russchristensen@Russs-MacBook-Pro.local>
2016-08-23 12:56:28 -0600

    Hello World

---
+++ Warmup/WarmupTests/WarmupTests.swift
@@ -0,0 +1,36 @@
+//
+//  WarmupTests.swift
+//  WarmupTests
+//
+//  Created by Russ Christensen on 8/23/16.
+//  Copyright © 2016 Russ Christensen. All rights reserved.
+//
+
+import XCTest
+@testable import Warmup
+
+class WarmupTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
